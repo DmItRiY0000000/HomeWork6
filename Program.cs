0bd6dea10468e814b43c02cc2587ee4e158d757c
Dmitriy <dmit.costin2013@yandex.ru>
2022-09-17 20:52:20 +0300

    HomeWork6

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int x)
+{
+for (int i = 0; i < x; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if(massiveNumbers[i] > 0 ) 
+        count ++ ; 
+    }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+/*
+void CrossPoint(double k1, double b1, double k2, double b2)
+{
+double x = (b1-b2)/(k2-k1);  // -2/4
+double y = (k2*b1-k1*b2)/(k2-k1);  // -2/4
+if(k1==k2) 
+Console.Write("Заданные прямые не пересекаются!");
+else
+Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+}
+
+Console.WriteLine("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+CrossPoint(k1,b1,k2,b2);
+
+*/
